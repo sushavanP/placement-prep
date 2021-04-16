d65b8a70dcf9f475108e6a9adbe8aa0b215de1ad
sushavanP <sushavan.189201016@muj.manipal.edu>
2021-04-16 21:17:19 +0530

    pushed naturalNumSum and checkPalindromeString

---
+++ src/recursion/naturalNumSum.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class naturalNumSum {
+    public static int getSum(int n){
+        if(n == 0)
+            return 0;
+        return n + getSum(n - 1);
+    }
+    public static void main(String[] args){
+        System.out.println(getSum(6));
+    }
+}
