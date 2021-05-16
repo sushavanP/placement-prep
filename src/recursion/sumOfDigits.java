8070b50c412f742856a4f64b9ecf1dec3397cf6b
sushavanP <sushavan.189201016@muj.manipal.edu>
2021-05-16 20:59:16 +0530

    sum of digits using recursion

---
+++ src/recursion/sumOfDigits.java
@@ -0,0 +1,11 @@
+package recursion;
+
+public class sumOfDigits {
+    public static int sum(int n){
+        if(n == 0) return 0;
+        return sum(n/10) + n%10;
+    }
+    public static void main(String[] args){
+        System.out.println(sum(873));
+    }
+}
